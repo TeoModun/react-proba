6815091246aebe2f8cb44d07931677b4d4bba893
TeoModun <49363476+TeoModun@users.noreply.github.com>
2020-05-07 23:54:55 +0200

    Updates

---
+++ precache-manifest.abf2099c72e672d42ef3a874e4309486.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c760db3df744230d84c7847430273a1a",
+    "url": "/react-proba/index.html"
+  },
+  {
+    "revision": "e74e1c085290f1bcb045",
+    "url": "/react-proba/static/css/main.8cae1ee9.chunk.css"
+  },
+  {
+    "revision": "3700d65cdad8397210aa",
+    "url": "/react-proba/static/js/2.68c2882a.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/react-proba/static/js/2.68c2882a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e74e1c085290f1bcb045",
+    "url": "/react-proba/static/js/main.d651b18a.chunk.js"
+  },
+  {
+    "revision": "79c4454e0e49b46b4314",
+    "url": "/react-proba/static/js/runtime-main.142b7027.js"
+  },
+  {
+    "revision": "b0ee7b8a3b8c19a26e1d0c8964c8a6cf",
+    "url": "/react-proba/static/media/slika.b0ee7b8a.jpeg"
+  }
+]);
